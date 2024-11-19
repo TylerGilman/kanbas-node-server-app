db7b3b583b50b8681eb2c715fe13fbbd645ec62f
TylerGilman <tylersgilman@gmail.com>
2024-11-18 22:16:13 -0500

    4.2

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
 const app = express();
 // Routers
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
