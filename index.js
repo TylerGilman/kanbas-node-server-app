fbfc53d8db328d9f3b4659e2c74d57e678ec88ab
TylerGilman <tylersgilman@gmail.com>
2024-11-18 19:59:14 -0500

    3.5.6

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 const app = express();
 // Routers
+app.use(cors());
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
