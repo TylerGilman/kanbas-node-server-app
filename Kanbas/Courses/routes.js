c0ae79967f8d91fff510bd8b768e8abc692aba1b
TylerGilman <tylersgilman@gmail.com>
2024-11-21 12:20:21 -0500

    4.5.3

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,5 +1,12 @@
 import * as dao from "./dao.js";
+import * as modulesDao from "../Modules/dao.js";
 export default function CourseRoutes(app) {
+  app.get("/api/courses/:courseId/modules", (req, res) => {
+    const { courseId } = req.params;
+    const modules = modulesDao.findModulesForCourse(courseId);
+    res.json(modules);
+  });
+
   app.get("/api/courses", (req, res) => {
     const courses = dao.findAllCourses();
     res.send(courses);
@@ -16,5 +23,15 @@ export default function CourseRoutes(app) {
     const courseUpdates = req.body;
     const status = dao.updateCourse(courseId, courseUpdates);
     res.send(status);
+  });
+
+  app.post("/api/courses/:courseId/modules", (req, res) => {
+    const { courseId } = req.params;
+    const module = {
+      ...req.body,
+      course: courseId,
+    };
+    const newModule = modulesDao.createModule(module);
+    res.send(newModule);
   });
 }
