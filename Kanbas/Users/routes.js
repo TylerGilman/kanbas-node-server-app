6715a7d68bf9ad48103e8fa134cab0334e65698c
TylerGilman <tylersgilman@gmail.com>
2024-12-09 02:50:41 -0500

    removed node modules

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -5,6 +5,17 @@ export default function UserRoutes(app) {
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
   const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
     const users = await dao.findAllUsers();
     res.json(users);
   };
@@ -73,6 +84,7 @@ export default function UserRoutes(app) {
     const courses = await courseDao.findCoursesForEnrolledUser(userId);
     res.json(courses);
   };
+
   app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 
   const createCourse = async (req, res) => {
